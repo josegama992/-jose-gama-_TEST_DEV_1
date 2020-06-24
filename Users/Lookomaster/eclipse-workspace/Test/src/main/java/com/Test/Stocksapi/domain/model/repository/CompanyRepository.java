2d8ab37125bed0b941906b5119dc9e700a1cdde3
josegama992 <lookomaster@hotmail.com>
2020-06-24 12:18:07 -0300

    Revert "restored files"
    This reverts commit e6f82e4c8ed7c2d2c8a70395896479d2a5b896c7.

--- Users/Lookomaster/eclipse-workspace/Test/src/main/java/com/Test/Stocksapi/domain/model/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Test.Stocksapi.domain.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Test.Stocksapi.domain.model.Company;
-
-public interface CompanyRepository extends JpaRepository<Company, Long>{
-
-}
